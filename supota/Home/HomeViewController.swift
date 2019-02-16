80f84a8c317d53cbda59f678a2c97c429ca42245
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-16 13:06:54 +0900

    Update

---
+++ supota/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  supota
+//
+//  Created by 築山朋紀 on 2019/02/16.
+//  Copyright © 2019 柳川優稀. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
